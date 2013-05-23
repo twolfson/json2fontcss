db126554492ccd7ba7e9547357ba69917ea7bc01
Todd Wolfson <todd@twolfson.com>
2013-05-23 02:21:49 -0700

    Generating tests based off of json2css

---
+++ test/json2fontcss_outline.js
@@ -0,0 +1,16 @@
+module.exports = {
+  'An array of characters and names': {
+    'processed into JSON': {
+      'matches as expected': true,
+      'is valid JSON': true
+    },
+    'processed into Stylus': {
+      'matches as expected': true,
+      'is valid Stylus': true
+    },
+    'processed into LESS': {
+      'matches as expected': true,
+      'is valid LESS': true
+    }
+  }
+};
