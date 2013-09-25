036b5b0d4072e59dc1b3982b3f659c2cbfcd6940
Luke Brooker <mail@lukebrooker.com>
2013-09-25 13:29:25 +1000

    Ported template to SCSS

--- test/json2fontcss_content.js
+++ test/json2fontcss_content.js
@@ -121,7 +121,7 @@ module.exports = {
       // Callback
       done(err);
     });
-  // },
+  },
 
   // // SASS
   // 'processed into SASS': [function () {
@@ -153,35 +153,35 @@ module.exports = {
   //   });
   // },
 
-  // // SCSS
-  // 'processed into SCSS': [function () {
-  //   this.options = {'format': 'scss'};
-  //   this.filename = 'scss.scss';
-  // }, 'processed via json2fontcss'],
-  // 'is valid SCSS': function (done) {
-  //   // Add some SCSS to our result
-  //   var scssStr = this.result;
-  //   scssStr += '\n' + [
-  //     '.feature {',
-  //     '  height: $sprite1-height;',
-  //     '  @include sprite-width($sprite2);',
-  //     '  @include sprite-image($sprite3);',
-  //     '}',
-  //     '',
-  //     '.feature2 {',
-  //     '  @include sprite($sprite2);',
-  //     '}'
-  //   ].join('\n');
+  // SCSS
+  'processed into SCSS': [function () {
+    this.options = {'format': 'scss'};
+    this.filename = 'scss.scss';
+  }, 'processed via json2fontcss'],
+  'is valid SCSS': function (done) {
+    // Add some SCSS to our result
+    var scssStr = this.result;
+    scssStr += '\n' + [
+      '.feature {',
+      '  height: $sprite1-height;',
+      '  @include sprite-width($sprite2);',
+      '  @include sprite-image($sprite3);',
+      '}',
+      '',
+      '.feature2 {',
+      '  @include sprite($sprite2);',
+      '}'
+    ].join('\n');
 
-  //   // Render the SCSS, assert no errors, and valid CSS
-  //   var tmp = new Tempfile();
-  //   tmp.writeFileSync(scssStr);
-  //   exec('sass --scss ' + tmp.path, function (err, css, stderr) {
-  //     assert.strictEqual(stderr, '');
-  //     assert.strictEqual(err, null);
-  //     assert.notEqual(css, '');
-  //     // console.log('SCSS', css);
-  //     done(err);
-  //   });
+    // Render the SCSS, assert no errors, and valid CSS
+    var tmp = new Tempfile();
+    tmp.writeFileSync(scssStr);
+    exec('sass --scss ' + tmp.path, function (err, css, stderr) {
+      assert.strictEqual(stderr, '');
+      assert.strictEqual(err, null);
+      assert.notEqual(css, '');
+      // console.log('SCSS', css);
+      done(err);
+    });
   }
 };
